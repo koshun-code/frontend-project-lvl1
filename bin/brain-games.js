ae6757114ebbcd2f65f4cb691cbe2c10907d289c
koshun <sit.com.k@gmail.com>
2020-02-04 21:21:24 +0300

    i

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import * as index from '../src/index.js';
 console.log("Welcome to the Brain Games");
+const name = index.askName();
+console.log(`Hello, ${name}`);
